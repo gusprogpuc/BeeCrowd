7d3fdc49905a0078167bab03305e106946d40616
Haiden69 <gustavo.haiden69@gmail.com>
2025-07-04 15:33:36 -0300

    1016 pronto, 1017 criado

--- C/1016.c
+++ C/1016.c
@@ -2,7 +2,10 @@
 
 int main() 
 {
-    
+    int distancia,tempo;
+    scanf("%d",&distancia);
+    tempo = distancia*2;
+    printf("%d minutos\n",tempo);
 
     return 0;
 }
---
+++ C/1017.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    int tempo,velocidade;
+    scanf("%d %d",&tempo,&velocidade);
+    
+
+    return 0;
+}
